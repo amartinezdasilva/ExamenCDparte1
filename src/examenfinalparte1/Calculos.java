e5cad1c4fe2ef320d6e262ffb9d51ad1513a7154
Aaron <Aaron@DESKTOP-BCLEH27>
2016-06-06 09:29:21 +0200

    escrito los comentarios ahora hay que generar el javadoc

--- src/examenfinalparte1/Calculos.java
+++ src/examenfinalparte1/Calculos.java
@@ -1,21 +1,37 @@
 package examenfinalparte1;
-
+/**
+ * @author Aaron
+*/
 public class Calculos {
 
     int VariableCalculos;
-
+/**
+ * 
+ * @param j 
+ * @param m  
+ */
     public Calculos(int j, int m) {
         VariableCalculos = calculo(j, m);
     }
-
+/**
+ * 
+ * @param j 
+ */
     public Calculos(int j) {
         VariableCalculos = calculo(j, j);
     }
-
+/**
+ * @see Calculos da el valor '-1' a la variable VariableCalculos
+ */
     public Calculos() {
         VariableCalculos = -1;
     }
-
+/**
+ * 
+ * @param x
+ * @param x1
+ * @return 
+ */
     public int calculo(int x, int x1) {
         return x + x1;
     }
